9a1d7def2b0c3134ca231ece8e498937b2075f08
김대현 <dhrte@tukorea.ac.kr>
2025-05-20 00:59:50 +0900

    wordListActivity wordViewActivity 추가
    관련 XML 수정

--- app/src/main/java/com/example/wordbook/wordListActivity.kt
+++ app/src/main/java/com/example/wordbook/wordListActivity.kt
@@ -1,10 +1,13 @@
 package com.example.wordbook
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,7 @@ import com.example.wordbook.databinding.WordLayoutBinding
 import com.example.wordbook.databinding.WordListBinding
 
 class wordListActivity : AppCompatActivity() {
+    lateinit var adapter: MyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,10 +36,32 @@ class wordListActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val datas = mutableListOf(WordItem("test", "테스트"))
+        binding.searchBarImage.setOnClickListener {
+            binding.search.requestFocus()
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(binding.search, InputMethodManager.SHOW_IMPLICIT)
+        }
 
+
+        val datas = mutableListOf(
+            WordItem("test", "테스트"),
+            WordItem("test2", "테스트2")
+        )
+
+        adapter = MyAdapter(datas)
         binding.recycler.layoutManager = LinearLayoutManager(this)
-        binding.recycler.adapter = MyAdapter(datas)
+        binding.recycler.adapter = adapter
+
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        if (ev.action == MotionEvent.ACTION_DOWN) {
+            adapter.closeEditContainer()
+        }
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+
+        return super.dispatchTouchEvent(ev)
     }
 }
 
@@ -48,6 +74,9 @@ class MyViewHolder(val binding:WordLayoutBinding): RecyclerView.ViewHolder(bindi
 
 class MyAdapter(val datas: MutableList<WordItem>):
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    var editContainerOpened: View? = null
+
     override fun getItemCount(): Int = datas.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
@@ -69,20 +98,25 @@ class MyAdapter(val datas: MutableList<WordItem>):
 
         binding.wordContainer.setOnLongClickListener {
             val container = binding.editContainer
+
             if (container.visibility == View.GONE) {
+                editContainerOpened?.visibility = View.GONE
                 container.visibility = View.VISIBLE
+                editContainerOpened = container
                 container.alpha = 0f
                 container.translationY = -20f
                 container.animate()
                     .alpha(1f)
                     .translationY(0f)
+                    .scaleY(1f)
                     .setDuration(200)
                     .start()
             } else {
                 container.animate()
                     .alpha(0f)
-                    .translationY(-20f)
-                    .setDuration(200)
+                    .translationY(-10f)
+                    .scaleY(0f)
+                    .setDuration(100)
                     .withEndAction {
                         container.visibility = View.GONE
                     }
@@ -90,5 +124,35 @@ class MyAdapter(val datas: MutableList<WordItem>):
             }
             true
         }
+
+        binding.wordContainer.setOnClickListener {
+            val container = binding.editContainer
+
+            if (container.visibility == View.VISIBLE)
+                container.animate()
+                    .alpha(0f)
+                    .translationY(-10f)
+                    .scaleY(0f)
+                    .setDuration(100)
+                    .withEndAction {
+                        container.visibility = View.GONE
+                    }
+                    .start()
+        }
     }
+
+    fun closeEditContainer() {
+        val container = editContainerOpened
+
+        container?.animate()
+            ?.alpha(0f)
+            ?.translationY(-10f)
+            ?.scaleY(0f)
+            ?.setDuration(100)
+            ?.withEndAction {
+                container?.visibility = View.GONE
+            }
+            ?.start()
+    }
+
 }
